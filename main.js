e5f1295d9d1dac6870f142b7ce36ab38bbe4f0db
Henrique Pompeu <75734520+Raewe@users.noreply.github.com>
2024-01-31 13:21:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function toggleMode() {
   const image = document.querySelector(".profile img");
 
   if (html.classList.contains("light")) {
-    image.setAttribute("src", "./images/avatar.png");
+    image.setAttribute("src", "./Images/avatar.png");
     image.setAttribute("alt", "Foto de perfil do modo dark");
   } else {
-    image.setAttribute("src", "./images/avatar-light.png");
+    image.setAttribute("src", "./Images/avatar-light.png");
     image.setAttribute("alt", "Foto de perfil do modo light");
   }
 
